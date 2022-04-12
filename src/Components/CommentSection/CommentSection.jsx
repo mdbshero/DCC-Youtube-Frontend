219addcb673cc430c91e350e710906c20aefc9e2
Garett Bare <Garett.Bare@yahoo.com>
2022-04-11 19:31:03 -0500

    removed the backup version of comment section

--- src/Components/CommentSection/CommentSection.jsx
+++ src/Components/CommentSection/CommentSection.jsx
@@ -1,64 +1,52 @@
 import React, { useEffect, useState } from "react";
 import AddComment from "./AddComment/AddComment";
+import AddReply from "./AddReply/AddReply";
 import axios from "axios";
 
 const CommentSection = (props) => {
   const [newComment, setNewComment] = useState();
-  const [comments, setComments] = useState([''])
+  const [newReply, setNewReply] = useState();
+  // const [comments, setComments] = useState([''])
+  // const [comments, setComments] = useState()
     
 
-  const commentSniffer =  () => {
-    let text = []
-    // let commentSection = await axios.get(`http://localhost:3001/api/${props.videoId}`)
-    // let commentSection = axios.get(`http://localhost:3001/api/1`)
-    axios.get(`http://localhost:3001/api/aWzlQ2N6qqg`)
-    .then((test) => {
-      // console.log(test.data)
-      // console.log(test)
-      for (let i = 0; i < test.data.length; i++){
-        // console.log(test.data[i].text)
-        text.push(test.data[i].text)
-        // setComments([...comments, commentSection.data[i].text], console.log(`comments: ${comments}`))
-      }
-      // let example = [...text]
-      // console.log(`Example: ${example}`)
-      // console.log(`text: ${text}`)
-      // setMovies(prevMovies => ([...prevMovies, ...result]));
-      console.log(text)
-      setComments(text =>  ([...text]))
-      console.log(comments)
-      
-      // setComments(example,console.log(comments))
-      // console.log(test)
-      // console.log(`comments: ${comments}`)
-    })
-    return (text)
-  }
+  // const commentSniffer = async () => {
+  //   let text = []
+  //   let commentSection = await axios.get(`http://localhost:3001/api/${props.videoId}`)
+  //   for (let i = 0; i < commentSection.data.length; i++){
+  //     text.push(commentSection.data[i].text)
+  //   }
+  //   console.log(`text: ${text}`)
+  //   setComments([...text])
+  // }
 
-useEffect(
-  commentSniffer()
-    // let commentsTemp = await commentSniffer();
-    // console.log(commentsTemp)
-    // setComments([...commentsTemp])
-    // console.log(`comments: ${comments}`)
-    // console.log(comments)
-    );
+  
+  useEffect(() => {
+    props.commentSniffer()
+  }, [])
+
 
 function handleComments(text){
 }
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(comments)
-    // commentSniffer()
-    // const addComment = {
-    //   comment: newComment.comment,
-    //   videoId: newComment.videoId
-    // };
-    // axios.post("http://localhost:3001/api/", addComment).then((response) => {
-    //   console.log(response.status);
-    //   console.log(response.data.token);
-    // });
+    const addComment = {
+      text: newComment,
+      videoId: props.videoId
+    };
+
+    await axios.post("http://localhost:3001/api/", addComment).then((response) => {
+      console.log(response.status);
+      console.log(response.data.token);
+    });
+
+    document.getElementById('commentInputField').value = ''
+    props.commentSniffer()
+  };
+
+  const handleSubmitReply = async (e) => {
+    e.preventDefault();
   };
 
   return (
@@ -70,12 +58,16 @@ function handleComments(text){
         <thead>
           <tr>
             <th>Comments</th>
+            <th>Replies</th>
           </tr>
         </thead>
         <tbody>
-          {/* {comments.map((comment, index) => {
-            return <tr key={index}>{comment.comment}</tr>;
-          })} */}
+          {props.comments.map((comment, index) => {
+           return <tr key={index}>
+             <td>{comment.text}</td>
+             <td> <AddReply handleSubmitReply={handleSubmitReply} index={comment.key} /> </td>
+             </tr>;
+          })}
         </tbody>
       </table>
     </div>
